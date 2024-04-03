21f416f18cc833bf29b036221479862fb1b81946
MartinaMamdouh <martina.mamdou@gmail.com>
2024-04-03 13:45:53 +0200

    navigate immediatly if scaned

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-const DetailsScreen = () => {
-
+const DetailsScreen = ({ route }) => {
+    const ticket  = route.params;
+console.log("route details", ticket)
     const navigation = useNavigation();
     const navigateToScanner = () => {
         navigation.navigate('Scanner');
-      };
-      
+    };
+
     return (
         <View style={styles.container}>
-          <Text>Details</Text>
-          <Button title="Scan Again" onPress={navigateToScanner} />          
+            <Text>Details</Text>
+            <Text>Username: {ticket.username}</Text>
+            <Text>Ticket Number: {ticket.ticketNumber}</Text>
+            <Text>Is Entered: {ticket.isEntered ? 'Yes' : 'No'}</Text>
+            <Button title="Scan Again" onPress={navigateToScanner} />
         </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-      },
-      space: {
-        height: 10, 
-      },
-    });
-    
-    export default DetailsScreen;
-    
+    },
+    space: {
+        height: 10,
+    },
+});
+
+export default DetailsScreen;
+
--- src/screens/ScannerScreen.tsx
+++ src/screens/ScannerScreen.tsx
@@ -106,25 +106,53 @@
 // export default ScannerScreen;
 
 
-import React, { useEffect, useState, useCallback } from 'react';
-import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, ToastAndroid } from 'react-native';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, ToastAndroid, Button } from 'react-native';
 import { Camera, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect, useIsFocused } from '@react-navigation/native';
 
 export function ScannerScreen() {
     const [torchOn, setTorchOn] = useState(false);
     const { hasPermission: cameraHasPermission, requestPermission: requestCameraPermission } = useCameraPermission();
     const backCamera = useCameraDevice('back');
-    const [scanning, setScanning] = useState(true);
+    const [isCameraActive, setIsCameraActive] = useState(true);
     const [scannedCodes, setScannedCodes] = useState(new Set());
     const navigation = useNavigation();
+    const [tickets, setTickets] = useState([
+        { username: 'User1', ticketNumber: 'T1', isEntered: false },
+        { username: 'User2', ticketNumber: 'T2', isEntered: false },
+        // Add more dummy data as needed
+    ]);
 
-    const handleCameraError = useCallback(() => {
-    }, []);
+    const isFocused = useIsFocused();
+
+    // Use this to determine the focus state of the screen
+    useFocusEffect(
+        useCallback(() => {
+            if (isFocused) {
+                setIsCameraActive(true);
+                console.log("isCameraActive ",isCameraActive);
+
+                console.log("focus"); //enable the scanner when the screen is focused
+            } else {
+                setIsCameraActive(false);
+                console.log("isCameraActive",isCameraActive);
+                console.log("not focus"); // disable the scanner when the screen is unfocused
+            }
+            // setIsCameraActive(true);
+            // console.log("focus"); //enable the scanner when the screen is focused
+            // return () => {
+
+            //     setIsCameraActive(false);
+            //     console.log("not focus"); // disable the scanner when the screen is unfocused
+            // };
+
+        }, [isFocused])
+    );
 
     useEffect(() => {
         const handleCameraPermission = async () => {
-            console.log("check permission to camera")
+            console.log("check permission to camera");
             const granted = await requestCameraPermission();
             if (!granted) {
                 Alert.alert('Camera permission is required to use the camera. Please grant permission in your device settings.');
@@ -132,22 +160,98 @@ export function ScannerScreen() {
         };
         handleCameraPermission();
     }, []);
-   
+
+    const toggleEntered = (index) => {
+        setTickets((prevTickets) =>
+            prevTickets.map((ticket, i) =>
+                i === index ? { ...ticket, isEntered: !ticket.isEntered } : ticket
+            )
+        );
+    };
+    const handleCameraError = useCallback(() => {
+    }, []);
+
+
+
+    function updateTicket(ticketNumber: string, isEntered: boolean) {
+        // Find the index of the ticket to update
+        const index = tickets.findIndex(ticket => ticket.ticketNumber === ticketNumber);
+
+        // Create a new array with the updated ticket
+        const updatedTickets = [...tickets];
+        if (index !== -1) {
+            updatedTickets[index] = { ...updatedTickets[index], isEntered };
+        }
+
+        // Update the state with the new array
+        setTickets(updatedTickets);
+    }
+
     const codeScanner = useCodeScanner({
-        codeTypes: ['qr', 'ean-13'],
+        codeTypes: ['qr', 'ean-13'],  //to recognize qrcode and barcode
         onCodeScanned: (codes) => {
-            const code = codes[0];
+            try {
+                const code = codes[0];
+                const ticket = tickets.find(ticket => ticket.ticketNumber === code.value);
 
-            if (code && !scannedCodes.has(code.value)) {
-                setScannedCodes((prev) => new Set([...prev, code.value]));
-
-                // Alert.alert(`QR Code Scanned: ${code.value}`);
-                Alert.alert(`QR Code Scanned: ${code.value}`, 'Scan another?', [
-                    { text: 'Yes', onPress: () => { } }, // No action needed
-                    { text: 'No', onPress: () => setScannedCodes(new Set()) }, // Clear scanned codes
-                    { text: 'Details', onPress: () => navigation.navigate('Details') }
-                ]);
+                if (ticket) {
+                    updateTicket(code.value!, true);
+                    console.log("ticket ", ticket);
+                    navigation.navigate('Details', ticket);
+                }
+            } catch (error) {
+                console.error("Error scanning code:", error);
             }
+
+
+            // if (ticket && ticket.isEntered == false) {
+            //     setTickets(tickets.map(t => t.ticketNumber === code.value ? { ...t, isEntered: true } : t));
+            //         const updatedTicket = { ...ticket, isEntered: true };
+            //         console.log("updated ticket: ", updatedTicket);
+            //         navigation.navigate('Details', updatedTicket );
+            // }
+
+
+
+            // if(!scannedCodes.has(code.value)){
+            //     if (ticket && ticket.isEntered == false) {
+            //         console.log("ticket: ",ticket)
+            //         setTickets(tickets.map(t => t.ticketNumber === code.value ? { ...t, isEntered: true } : t));
+            //         const updatedTicket = { ...ticket, isEntered: true };
+            //         console.log("updated ticket: ", updatedTicket);
+            //         Alert.alert(`QR Code Scanned:${code.value}`,`scan again?`,[
+            //             { text: 'Yes', onPress: () => {
+            //                 // setIsCameraActive(false);
+            //                 // setTimeout(() => setIsCameraActive(true), 100); // Delay to ensure the camera is deactivated
+            //                 } }, // No action needed
+            //             { text: 'No', onPress: () => navigation.navigate('Home') }, 
+            //             { text: 'Details', onPress: () => navigation.navigate('Details', updatedTicket ) }
+            //         ]);
+            //     }
+            //     else if (!ticket ) {
+            //         setScannedCodes((prev) => new Set([...prev, code.value]));
+            //         Alert.alert(`Ticket ${code.value} not found.`,`scan again?`,[
+            //             { text: 'Yes', onPress: () => { } }, // No action needed
+            //     { text: 'No', onPress: () => navigation.navigate('Home') }, ]);
+            //     }
+            // }else{
+            //     setScannedCodes((prev) => new Set([...prev, code.value]));
+            //     Alert.alert(`Code ${code.value} has already been scanned.`,`scan again?`,[
+            //         { text: 'Yes', onPress: () => { } }, // No action needed
+            // { text: 'No', onPress: () => navigation.navigate('Home') }, ]);
+            // }
+
+
+            // if (code && !scannedCodes.has(code.value)) {
+            //     setScannedCodes((prev) => new Set([...prev, code.value]));
+
+            //     // Alert.alert(`QR Code Scanned: ${code.value}`);
+            //     Alert.alert(`QR Code Scanned: ${code.value}`, 'Scan another?', [
+            //         { text: 'Yes', onPress: () => { } }, // No action needed
+            //         { text: 'No', onPress: () => setScannedCodes(new Set()) }, // Clear scanned codes
+            //         { text: 'Details', onPress: () => navigation.navigate('Details') }
+            //     ]);
+            // }
 
         },
     });
@@ -157,17 +261,19 @@ export function ScannerScreen() {
     }
     return (
         <SafeAreaView style={styles.container}>
-            <Camera
+            {isFocused && <Camera
                 codeScanner={codeScanner}
                 style={styles.camera}
                 // style={StyleSheet.absoluteFill}
                 device={backCamera}
-                isActive={true}
+                isActive={isCameraActive}
                 torch={torchOn ? 'on' : 'off'}
                 onError={handleCameraError}
                 resizeMode="cover"
-            />
-         
+                
+            />}
+
+<View style={styles.overlay} />
             <TouchableOpacity
                 style={styles.torchButton}
                 onPress={() => setTorchOn((prev) => !prev)}
@@ -198,7 +304,18 @@ const styles = StyleSheet.create({
     camera: {
         width: '100%',
         // flex: 1,
-aspectRatio: 4/3,
+        aspectRatio: 4 / 3,
+    },
+    overlay: {
+        position: 'absolute',
+        // top: '10%', // Adjust the position as needed
+        // left: '10%', // Adjust the position as needed
+        width: '60%', // Adjust the size as needed
+        height: '40%', // Adjust the size as needed
+        borderWidth: 2,
+        borderColor: '#00FF00', // Green border for the scanning area
+        borderRadius: 10, // Optional: for rounded corners
+        backgroundColor: 'rgba(0,0,0,0.5)', // Semi-transparent background
     },
 });
 export default ScannerScreen;
